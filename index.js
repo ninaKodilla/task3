d9acc788c3baf16c12d022c2bbc04b149ad4aebc
npupiec <nkozlowska89@gmail.com>
2018-05-23 09:58:58 +0200

    feature/task3

---
+++ index.js
@@ -0,0 +1,33 @@
+process.stdin.setEncoding('utf-8');
+
+var os = require('os');
+var OSinfo = require('./modules/OSinfo');
+var time = require('./modules/time');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+
+    if (input) {
+        switched(input);
+    }
+    
+    function switched(input) {
+        var instruction = input.trim();
+    
+        switch (instruction) {
+            case 'info':
+                process.stdout.write(process.versions.node);
+                process.stdout.write(process.env.lang);
+                process.exit();
+                break;
+            case '/getOSinfo':
+                OSinfo.print();
+                break;
+            case '/time':
+                time.print();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
+    }
+});
